425485233aea52554ff8f32f22c69464ae145366
Joël Schaerer <joel.schaerer@laposte.net>
2017-09-30 21:20:00 +0200

    doc

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Deploy the artilab website to OVH. Make sure to set ARTILAB_PWD before running it
+
 set +e
 echo "lftp ends with a segfault, but apart from that all seems to work properly"
 
